bcfe9dada7aa381300f27ced56794608c8d61f30
anurag0510 <anurag0510@outlook.com>
2021-06-02 20:16:16 +0530

    added tests for update user endpoint

--- src/test/java/org/logistics/entityService/controller/UserControllerTest.java
+++ src/test/java/org/logistics/entityService/controller/UserControllerTest.java
@@ -8,6 +8,7 @@ import org.logistics.entityService.data.UserEntity;
 import org.logistics.entityService.data.UsersRepository;
 import org.logistics.entityService.exceptions.UserServiceException;
 import org.logistics.entityService.model.request.CreateUserRequestModel;
+import org.logistics.entityService.model.request.UpdateUserRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -152,7 +153,7 @@ public class UserControllerTest {
         try {
             userController.getUser(true, "invalid_filter", "anurag0510@outlook.com");
         } catch (UserServiceException ex) {
-            Assertions.assertEquals("Only allowed to filter via : uid, userName, emailAddress", ex.getMessage());
+            Assertions.assertEquals("Only allowed to filter via : uid, user_name, email_address", ex.getMessage());
         }
     }
 
@@ -180,6 +181,139 @@ public class UserControllerTest {
             userController.getUser(false, "email_address", "USR-ddb39364-23f9-4571-af60-d29d6a84bab3");
         } catch (UserServiceException ex) {
             Assertions.assertEquals("No such user is present/active in system with email_address : USR-ddb39364-23f9-4571-af60-d29d6a84bab3", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void updateUserBasedOnUidTest() {
+        when(usersRepository.findAllActiveUsersWithUid("USR-ddb39364-23f9-4571-af60-d29d6a84bab3")).
+                thenReturn(new ArrayList<>(Arrays.asList(
+                        new UserEntity(1L, "USR-ddb39364-23f9-4571-af60-d29d6a84bab3", "anurag0510", "Anurag", "Dubey", "anurag0510@outlook.com", "91", "1231231234", "abrakadabra", "saltdabra", "Electronic City, Bangalore", "India", "Bangalore", new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), true, false, null)
+                )));
+        UpdateUserRequestModel userDetaisToBeUpdated = new UpdateUserRequestModel();
+        userDetaisToBeUpdated.setFirstName("Updated First");
+        userDetaisToBeUpdated.setLastName("Updated Last");
+        Assertions.assertEquals("Updated First", userController.updateUser( userDetaisToBeUpdated, "uid", "USR-ddb39364-23f9-4571-af60-d29d6a84bab3").getBody().getFirstName());
+        Assertions.assertEquals("Updated Last", userController.updateUser( userDetaisToBeUpdated, "uid", "USR-ddb39364-23f9-4571-af60-d29d6a84bab3").getBody().getLastName());
+    }
+
+    @Test
+    public void updateUserBasedOnUserNameTest() {
+        when(usersRepository.findAllActiveUsersWithUserName("anurag0510")).
+                thenReturn(new ArrayList<>(Arrays.asList(
+                        new UserEntity(1L, "USR-ddb39364-23f9-4571-af60-d29d6a84bab3", "anurag0510", "Anurag", "Dubey", "anurag0510@outlook.com", "91", "1231231234", "abrakadabra", "saltdabra", "Electronic City, Bangalore", "India", "Bangalore", new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), true, false, null)
+                )));
+        UpdateUserRequestModel userDetaisToBeUpdated = new UpdateUserRequestModel();
+        userDetaisToBeUpdated.setFirstName("Updated First");
+        userDetaisToBeUpdated.setLastName("Updated Last");
+        Assertions.assertEquals("Updated First", userController.updateUser( userDetaisToBeUpdated, "user_name", "anurag0510").getBody().getFirstName());
+        Assertions.assertEquals("Updated Last", userController.updateUser( userDetaisToBeUpdated, "user_name", "anurag0510").getBody().getLastName());
+    }
+
+    @Test
+    public void updateUserBasedOnEmailAddressTest() {
+        when(usersRepository.findAllActiveUsersWithEmailAddress("anurag0510@outlook.com")).
+                thenReturn(new ArrayList<>(Arrays.asList(
+                        new UserEntity(1L, "USR-ddb39364-23f9-4571-af60-d29d6a84bab3", "anurag0510", "Anurag", "Dubey", "anurag0510@outlook.com", "91", "1231231234", "abrakadabra", "saltdabra", "Electronic City, Bangalore", "India", "Bangalore", new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), true, false, null)
+                )));
+        UpdateUserRequestModel userDetaisToBeUpdated = new UpdateUserRequestModel();
+        userDetaisToBeUpdated.setFirstName("Updated First");
+        userDetaisToBeUpdated.setLastName("Updated Last");
+        Assertions.assertEquals("Updated First", userController.updateUser( userDetaisToBeUpdated, "email_address", "anurag0510@outlook.com").getBody().getFirstName());
+        Assertions.assertEquals("Updated Last", userController.updateUser( userDetaisToBeUpdated, "email_address", "anurag0510@outlook.com").getBody().getLastName());
+    }
+
+    @Test
+    public void updateUserWithPreExistingUserNameInSystemTest() {
+        when(usersRepository.findAllActiveUsersWithEmailAddress("anurag0510@outlook.com")).
+                thenReturn(new ArrayList<>(Arrays.asList(
+                        new UserEntity(1L, "USR-ddb39364-23f9-4571-af60-d29d6a84bab3", "anurag0510", "Anurag", "Dubey", "anurag0510@outlook.com", "91", "1231231234", "abrakadabra", "saltdabra", "Electronic City, Bangalore", "India", "Bangalore", new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), true, false, null)
+                )));
+        when(usersRepository.findAllActiveUsersWithUserName("anurag21st")).
+                thenReturn(new ArrayList<>(Arrays.asList(
+                        new UserEntity(1L, "USR-ddb39364-23f9-4571-af60-d29d6a84bae5", "anurag21st", "Anurag", "Dubey", "anurag@gmail.com", "91", "1231231237", "abrakadabra", "saltdabra", "Electronic City, Bangalore", "India", "Bangalore", new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), true, false, null)
+                )));
+        UpdateUserRequestModel userDetaisToBeUpdated = new UpdateUserRequestModel();
+        userDetaisToBeUpdated.setUserName("anurag21st");
+        try {
+            userController.updateUser( userDetaisToBeUpdated, "email_address", "anurag0510@outlook.com");
+        } catch (UserServiceException ex) {
+            Assertions.assertEquals("Can't update user with the user_name : anurag21st as system already has a user with same user_name present.", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void updateUserWithPreExistingEmailAddressInSystemTest() {
+        when(usersRepository.findAllActiveUsersWithUid("USR-ddb39364-23f9-4571-af60-d29d6a84bab3")).
+                thenReturn(new ArrayList<>(Arrays.asList(
+                        new UserEntity(1L, "USR-ddb39364-23f9-4571-af60-d29d6a84bab3", "anurag0510", "Anurag", "Dubey", "anurag0510@outlook.com", "91", "1231231234", "abrakadabra", "saltdabra", "Electronic City, Bangalore", "India", "Bangalore", new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), true, false, null)
+                )));
+        when(usersRepository.findAllActiveUsersWithEmailAddress("anurag@gmail.com")).
+                thenReturn(new ArrayList<>(Arrays.asList(
+                        new UserEntity(1L, "USR-ddb39364-23f9-4571-af60-d29d6a84bae5", "anurag21st", "Anurag", "Dubey", "anurag@gmail.com", "91", "1231231237", "abrakadabra", "saltdabra", "Electronic City, Bangalore", "India", "Bangalore", new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), true, false, null)
+                )));
+        UpdateUserRequestModel userDetaisToBeUpdated = new UpdateUserRequestModel();
+        userDetaisToBeUpdated.setEmailAddress("anurag@gmail.com");
+        try {
+            userController.updateUser( userDetaisToBeUpdated, "uid", "USR-ddb39364-23f9-4571-af60-d29d6a84bab3");
+        } catch (UserServiceException ex) {
+            Assertions.assertEquals("Can't update user with the email_address : anurag@gmail.com as system already has a user with same email address present.", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void updateUserWithPreExistingMobileNumberInSystemTest() {
+        when(usersRepository.findAllActiveUsersWithUid("USR-ddb39364-23f9-4571-af60-d29d6a84bab3")).
+                thenReturn(new ArrayList<>(Arrays.asList(
+                        new UserEntity(1L, "USR-ddb39364-23f9-4571-af60-d29d6a84bab3", "anurag0510", "Anurag", "Dubey", "anurag0510@outlook.com", "91", "1231231234", "abrakadabra", "saltdabra", "Electronic City, Bangalore", "India", "Bangalore", new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), true, false, null)
+                )));
+        when(usersRepository.findAllActiveUsersWithMobileNumber("91", "1231231237")).
+                thenReturn(new ArrayList<>(Arrays.asList(
+                        new UserEntity(1L, "USR-ddb39364-23f9-4571-af60-d29d6a84bae5", "anurag21st", "Anurag", "Dubey", "anurag@gmail.com", "91", "1231231237", "abrakadabra", "saltdabra", "Electronic City, Bangalore", "India", "Bangalore", new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), true, false, null)
+                )));
+        UpdateUserRequestModel userDetaisToBeUpdated = new UpdateUserRequestModel();
+        userDetaisToBeUpdated.setCountryCode("91");
+        userDetaisToBeUpdated.setMobileNumber("1231231237");
+        try {
+            userController.updateUser( userDetaisToBeUpdated, "uid", "USR-ddb39364-23f9-4571-af60-d29d6a84bab3");
+        } catch (UserServiceException ex) {
+            Assertions.assertEquals("Can't update user with the mobile_number : 1231231237 as system already has a user with same mobile number present.", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void updateUserBasedOnNonExistingUidTest() {
+        try {
+            userController.updateUser(new UpdateUserRequestModel(), "uid", "USR-ddb39364-23f9-4571-af60-d29d6a84bab3");
+        } catch (UserServiceException ex) {
+            Assertions.assertEquals("No user exist in system with uid : USR-ddb39364-23f9-4571-af60-d29d6a84bab3", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void updateUserBasedOnNonExistingUserNameTest() {
+        try {
+            userController.updateUser(new UpdateUserRequestModel(), "user_name", "USR-ddb39364-23f9-4571-af60-d29d6a84bab3");
+        } catch (UserServiceException ex) {
+            Assertions.assertEquals("No user exist in system with user_name : USR-ddb39364-23f9-4571-af60-d29d6a84bab3", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void updateUserBasedOnNonExistingEmailAddressTest() {
+        try {
+            userController.updateUser(new UpdateUserRequestModel(), "email_address", "USR-ddb39364-23f9-4571-af60-d29d6a84bab3");
+        } catch (UserServiceException ex) {
+            Assertions.assertEquals("No user exist in system with email_address : USR-ddb39364-23f9-4571-af60-d29d6a84bab3", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void updateUserBasedOnInvalidFilter() {
+        try {
+            userController.updateUser(new UpdateUserRequestModel(), "invalid_filter", "anurag0510@outlook.com");
+        } catch (UserServiceException ex) {
+            Assertions.assertEquals("Only allowed to filter via : uid, user_name, email_address", ex.getMessage());
         }
     }
 }
