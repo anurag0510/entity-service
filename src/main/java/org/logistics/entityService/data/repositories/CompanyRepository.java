a8a9eab4e41b75ef48cdacbdc7f506c923295570
anurag0510 <anurag0510@outlook.com>
2023-01-22 11:58:36 +0530

    moved CompanyRepository inherit from CrudRepo to JpaRepo

--- src/main/java/org/logistics/entityService/data/repositories/CompanyRepository.java
+++ src/main/java/org/logistics/entityService/data/repositories/CompanyRepository.java
@@ -2,12 +2,15 @@ package org.logistics.entityService.data.repositories;
 
 import org.logistics.entityService.data.entities.CompanyEntity;
 import org.logistics.entityService.data.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface CompanyRepository extends CrudRepository<CompanyEntity, Long> {
+import javax.transaction.Transactional;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
 
     @Query(value = "SELECT * FROM company c WHERE c.is_active = ?1 AND c.is_deleted = false",
             nativeQuery = true)
