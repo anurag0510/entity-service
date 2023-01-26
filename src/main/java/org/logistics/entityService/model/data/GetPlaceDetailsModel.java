cae67e074aeca5c17eb81aff5a7cf9eefbe64f78
anurag0510 <anurag0510@outlook.com>
2023-01-26 07:04:50 +0530

    get place details model defined

---
+++ src/main/java/org/logistics/entityService/model/data/GetPlaceDetailsModel.java
@@ -0,0 +1,17 @@
+package org.logistics.entityService.model.data;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetPlaceDetailsModel {
+
+    @JsonProperty("result")
+    GetPlaceDetailsResponseModel result;
+}
