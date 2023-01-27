ea79381c207922fb5da6d63687d3a90afd3d9b8b
anurag0510 <anurag0510@outlook.com>
2023-01-27 19:39:47 +0530

    geocode place model implemented

---
+++ src/main/java/org/logistics/entityService/model/data/GeocodePlaceDetails.java
@@ -0,0 +1,17 @@
+package org.logistics.entityService.model.data;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class GeocodePlaceDetails {
+
+    @JsonProperty("place_id")
+    String placeId;
+}
