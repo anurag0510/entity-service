ac1ad35f1e767d4edc16916690b2a3666ddd8eb6
anurag0510 <anurag0510@outlook.com>
2023-01-27 19:40:07 +0530

    basic geometry data model implemented

---
+++ src/main/java/org/logistics/entityService/model/data/GeometryData.java
@@ -0,0 +1,21 @@
+package org.logistics.entityService.model.data;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class GeometryData {
+
+    @JsonProperty("location")
+    LocationDetails location;
+
+    @JsonProperty("viewport")
+    ViewportDetails viewport;
+}
